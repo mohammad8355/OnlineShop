4add7981744563cc9185f0fd0af71dde085ede5e
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-11 07:32:47 -0800

    Add Brand Model

---
+++ BusinessEntity/Models/Brand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity.Models
+{
+    public class Brand
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [Required]
+        [MaxLength(300)]
+        public string Link { get; set; }
+    }
+
+}
