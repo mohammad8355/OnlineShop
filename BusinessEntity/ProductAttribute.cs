d0c5bc3d8928710c74445cffa3e3977670ee789d
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-11 10:48:40 -0800

    Add Product Attribute Model

---
+++ BusinessEntity/ProductAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntity.Models
+{
+    public class ProductAttribute
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string KeyName { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string KeyValue { get; set; }
+        [Required]
+        [MaxLength(150)]
+        public string Unit { get; set; }
+
+    }
+}
