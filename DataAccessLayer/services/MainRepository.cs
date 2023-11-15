e5cafbaddd69a7024ba39cc7aa96bb46af9caf76
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-15 14:17:17 +0330

    Add GenericRepository

---
+++ DataAccessLayer/services/MainRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.services
+{
+    public class MainRepository<TEntity> where TEntity : class
+    {
+        private readonly AppDbContext context;
+        public MainRepository(AppDbContext context)
+        {
+           this.context = context;
+        }
+        public void AddItem (TEntity entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+        public void EditItem(TEntity entity)
+        {
+            context.Update(entity);
+            context.SaveChanges();
+        }
+        public void DeleteItem(TEntity entity)
+        {
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+        public void DeleteItem(object Id)
+        {
+           var entity = context.Find(Id.GetType());
+            DeleteItem(entity);
+        }
+        public List<TEntity> GetAllItem()
+        {
+            return context.Set<TEntity>().ToList();
+        }
+
+    }
+
+}
