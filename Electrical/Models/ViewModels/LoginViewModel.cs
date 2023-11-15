a795fba325f5fa70b925bfdebd688184d30b7ea8
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-15 22:51:06 +0330

    Add Register And Login ViewModel

---
+++ Electrical/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Electrical.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("نام کاربری")]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [DisplayName("رمز عبور")]
+        public string Password { get; set; }
+    }
+}
---
+++ Electrical/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Electrical.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("نام کاربری")]
+        public string UserName { get; set; }
+        [Required]
+        [MaxLength(300)]
+        [DisplayName("ایمیل")]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [DisplayName("رمز عبور")]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [DisplayName("تکرار رمز عبور")]
+        [Compare("Password")]
+        public string ConfrimPassword { get; set; }
+
+    }
+}
