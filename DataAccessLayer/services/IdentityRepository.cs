9a46a89a60e421afb537026a45301cb87337d0e5
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-16 14:20:43 +0330

    Add identity repository

---
+++ DataAccessLayer/services/IdentityRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.services
+{
+    public class IdentityRepository
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public IdentityRepository(UserManager<IdentityUser> _userManager)
+        {
+            this._userManager = _userManager;
+        }
+        public async Task<IdentityResult> RegisterAsync(IdentityUser identityUser)
+        {
+          var resualt =  await _userManager.CreateAsync(identityUser);
+            return resualt;
+        }
+        //public async Task<IdentityResult> Login(IdentityUser identityUser)
+        //{
+    
+        //    return resualt;
+        //}
+    }
+}
