9f45801b4b5bff1c706d733bce11c7eec51be13d
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-16 14:21:00 +0330

    Add DBContext Factory

---
+++ DataAccessLayer/ApplicationDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+
+        public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+        {
+            public AppDbContext CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+                optionsBuilder.UseSqlServer("server=DESKTOP-5FTTMPU\\MSSQLSERVER2022;database=ElectricShopDB;Trusted_Connection=true;TrustServerCertificate=True");
+
+                return new AppDbContext(optionsBuilder.Options);
+            }
+        }
+}
