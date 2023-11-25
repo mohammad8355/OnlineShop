197bbb4d05d8baf78c0341fc1c18022eafaafc1d
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-11-25 20:46:22 +0330

    delete username set in email confrim

--- Electrical/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
+++ Electrical/Areas/Identity/Pages/Account/ConfirmEmailChange.cshtml.cs
@@ -54,13 +54,7 @@ namespace Electrical.Areas.Identity.Pages.Account
 
             // In our UI email and user name are one and the same, so when we update the email
             // we need to update the user name.
-            var setUserNameResult = await _userManager.SetUserNameAsync(user, user.UserName);
-            if (!setUserNameResult.Succeeded)
-            {
-                StatusMessage = "Error changing user name.";
-                return Page();
-            }
-
+ 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Thank you for confirming your email change.";
             return Page();
