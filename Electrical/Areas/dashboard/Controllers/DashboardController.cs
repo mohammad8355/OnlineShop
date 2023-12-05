9e7f9ec5a2177af64e97dbe39ccfe802e89be63e
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-05 09:31:28 +0330

    add dashboard area with controllers and views

---
+++ Electrical/Areas/dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("dashboard")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
