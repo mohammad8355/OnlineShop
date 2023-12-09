8758e3e00176ae47425cbeaacfbac146be4a872b
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-09 19:04:25 +0330

    Add discount controller

---
+++ Electrical/Areas/dashboard/Controllers/DiscountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class DiscountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddDiscount()
+        {
+            return View();
+        }
+    }
+}
