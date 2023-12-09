0938f65ec7f6e80b2106fc28f55591636946a267
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-09 19:03:55 +0330

    add technical specification controller

---
+++ Electrical/Areas/dashboard/Controllers/TechnicalSpecificationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("dashboard")]
+
+    public class TechnicalSpecificationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddTechnicalSpecification()
+        {
+            return View();
+        }
+    }
+}
