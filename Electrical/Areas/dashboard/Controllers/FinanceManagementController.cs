d8ab6a3bc5d78d882db95a41ca7454c370106c4e
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-14 19:32:10 +0330

    add finance management controller with views

---
+++ Electrical/Areas/dashboard/Controllers/FinanceManagementController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class FinanceManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
