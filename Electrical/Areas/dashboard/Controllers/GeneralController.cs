4f44987b0c928434917fa4e3af76496d735fff41
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-15 20:42:40 +0330

    Add general controller

---
+++ Electrical/Areas/dashboard/Controllers/GeneralController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class GeneralController : Controller
+    {
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+        public IActionResult ContactList()
+        {
+            return View();
+        }
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+        public IActionResult SliderList()
+        {
+            return View();
+        }
+        public IActionResult AddSlider()
+        {
+            return View();
+        }
+    }
+}
