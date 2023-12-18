ba611edda450cded898f66454de93dbef5df3538
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-18 20:13:19 +0330

    last stable version of project (finish admin interface in 90%)

---
+++ Electrical/Areas/dashboard/Controllers/Weblog.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("dashboard")]
+    public class Weblog : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddWeblog()
+        {
+            return View();
+        }
+    }
+}
