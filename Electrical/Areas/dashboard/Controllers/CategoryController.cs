8faa05ab435dba55239a55b4351d28db6037b807
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-18 20:11:18 +0330

    add category controller with views

---
+++ Electrical/Areas/dashboard/Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Areas.dashboard.Controllers
+{
+    [Area("dashboard")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddCategory() 
+        {
+            return View();
+        }
+
+    }
+}
