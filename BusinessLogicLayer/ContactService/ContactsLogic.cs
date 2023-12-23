a736e8cc20942102f968ffc57a036ed1237dbc86
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-23 20:07:05 +0330

    add contact logic methods

---
+++ BusinessLogicLayer/ContactService/ContactsLogic.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntity;
+namespace BusinessLogicLayer.ContactService
+{
+    public class ContactsLogic
+    {
+        private readonly MainRepository<Contact> ContactRepository;
+        public ContactsLogic(MainRepository<Contact> ContactRepository)
+        {
+            this.ContactRepository = ContactRepository;
+        }
+    }
+}
