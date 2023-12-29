613745f904343c04c5a7badde94152e4db104c53
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2023-12-29 15:05:28 +0330

    add add and edit subcategory viewmodel

---
+++ Electrical/Models/ViewModels/AddEditSubCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using BusinessEntity;
+
+namespace PresentationLayer.Models.ViewModels
+{
+    public class AddEditSubCategoryViewModel
+    {
+        public ICollection<Category> categories { get; set; }
+        public SubCategory subCategory { get; set; }
+    }
+}
