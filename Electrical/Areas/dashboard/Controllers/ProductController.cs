3c703ac4a4a194db8bb5a083bb1753449ecac110
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2024-01-04 12:20:52 +0330

    complete crud functions but one little error eccurce when edit product

--- Electrical/Areas/dashboard/Controllers/ProductController.cs
+++ Electrical/Areas/dashboard/Controllers/ProductController.cs
@@ -16,58 +16,93 @@ namespace PresentationLayer.Areas.dashboard.Controllers
         private readonly CategoryLogic categoryLogic;
         private readonly ProductLogic productLogic;
         private readonly SubCategoryLogic subcategoryLogic;
-        private readonly UploadFile uploadImage;
+        private readonly UploadFile fileManager;
         private readonly ProductPhotoLogic productPhotoLogic;
-        public ProductController(CategoryLogic categoryLogic, ProductLogic productLogic,SubCategoryLogic SubCategoryLogic, UploadFile uploadImage, ProductPhotoLogic productPhotoLogic)
+        public ProductController(CategoryLogic categoryLogic, ProductLogic productLogic, SubCategoryLogic SubCategoryLogic, UploadFile fileManager, ProductPhotoLogic productPhotoLogic)
         {
             this.productLogic = productLogic;
             this.categoryLogic = categoryLogic;
             subcategoryLogic = SubCategoryLogic;
-            this.uploadImage = uploadImage;
+            this.fileManager = fileManager;
             this.productPhotoLogic = productPhotoLogic;
 
         }
+        //list of product
         public IActionResult Index()
         {
+            #region bind view model
             var model = new ProductListViewModel()
             {
                 products = productLogic.ProductList(),
             };
+            #endregion
             return View(model);
         }
+        //add product  view 
         [HttpGet]
         public IActionResult AddProduct()
         {
+            #region bind addedit View Model
             var model = new AddEditProductViewModel()
             {
                 Categories = (List<Category>)categoryLogic.CategoryList(),
             };
+            #endregion
+
+            #region create select list of categories
             ViewBag.categories = new SelectList(model.Categories, "Id", "Name");
+            #endregion
             return View(model);
         }
+        //get model and add product
         [HttpPost]
         public async Task<IActionResult> AddProduct(AddEditProductViewModel model)
         {
-            //bind subcategory class to product model by use SubCategoryDetail method 
-            model.Product.subCategory = subcategoryLogic.SubCategoryDetail(model.Product.SubCategory_Id);
+            //in future this formats list would be dynamic
+            #region regular image formats list 
+            var formats = new List<string>()
+            {
+                ".jpg",
+                ".jpeg",
+                ".jfif",
+                ".png",
+            };
+            #endregion
+
+            #region check recevied format files
+            foreach (var file in model.File)
+            {
+                var formatFileCheck = fileManager.FormatChecker(file, formats);
+                if (!formatFileCheck)
+                {
+                    ModelState.AddModelError("", $"فایل با نام {file.FileName} فرمت های محاز را ندارد");
+                    return View(model);
+                }
+            }
+            #endregion
             //add product to db
             var resault = await productLogic.AddProduct(model.Product);
             if (resault)
             {
+                #region set requried parameters such as limitsize and destination of files
                 var limitsize = 0;
                 var destination = "Image\\Product\\" + model.Product.subCategory.Parent + "\\" + model.Product.subCategory.IdentifierName + "\\" + model.Product.Name;
-                //foreach on images files
+                #endregion
+
+                #region upload image files 
                 foreach (var file in model.File)
                 {
                     //make uniqe name
                     var ImageName = Guid.NewGuid().ToString();
                     //upload image on server;
-                    var UploadFileResault = await uploadImage.UploadImage(ImageName, destination, limitsize, "", file);
+
+                    var UploadFileResault = await fileManager.fileManager(ImageName, destination, limitsize, "", file);
                     //if upload successfull 
                     if ((bool)UploadFileResault.First() == true)
                     {
                         //convert bytes to kilobytes
                         var ImageSize = file.Length / 1024;
+                        #region bind productphoto model 
                         ProductPhoto productPhoto = new ProductPhoto()
                         {
                             Name = ImageName + UploadFileResault.Last().ToString(),
@@ -75,6 +110,7 @@ namespace PresentationLayer.Areas.dashboard.Controllers
                             Product_Id = model.Product.Id,
                             format = UploadFileResault.Last().ToString(),
                         };
+                        #endregion
                         //add photo to productphoto model
                         await productPhotoLogic.AddProductPhoto(productPhoto);
 
@@ -86,6 +122,7 @@ namespace PresentationLayer.Areas.dashboard.Controllers
                     }
 
                 }
+                #endregion
                 return RedirectToAction("Index");
             }
             else
@@ -93,22 +130,153 @@ namespace PresentationLayer.Areas.dashboard.Controllers
                 ModelState.AddModelError("", "عملیات افزودن با شکست مواجه شد");
                 return View(model);
             }
-            
+
 
         }
         [HttpGet("Product/SubCategoryDropDown/Id")]
         public JsonResult SubCategoryDropDown(int category_Id)
         {
+            #region make list of categories and subcategories for create select list
             var category = categoryLogic.CategoryDetail(category_Id);
             var subcategories = subcategoryLogic.SubCategoryList().Where(s => s.Parent == category.IdentifierName).ToList();
+            #endregion
             return Json(new SelectList(subcategories, "Id", "Name"));
-
         }
         [HttpGet]
         public async Task<IActionResult> ProductDetails(int Id)
         {
             var model = await productLogic.ProductDetail(Id);
             return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ProductEdit(int Id)
+        {
+            #region bind add edit view model 
+            var model = new AddEditProductViewModel()
+            {
+                Product = await productLogic.ProductDetail(Id),
+                Categories = (List<Category>)categoryLogic.CategoryList(),
+            };
+            #endregion
+            #region create select list of categories
+            ViewBag.categories = new SelectList(model.Categories, "Id", "Name", model.Product.subCategory.category_Id);
+            #endregion
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductEdit(AddEditProductViewModel model)
+        {
+            //in future this list must be dynamic thta admin can control formats
+            #region regular imagefiles formats
+            var formats = new List<string>()
+            {
+                ".jpg",
+                ".jpeg",
+                ".jfif",
+                ".png",
+            };
+            #endregion
+
+            if (model.File != null)
+            {
+                #region check format of image files
+                foreach (var file in model.File)
+                {
+                    var formatFileCheck = fileManager.FormatChecker(file, formats);
+                    if (!formatFileCheck)
+                    {
+                        ModelState.AddModelError("", $"فایل با نام {file.FileName} فرمت های محاز را ندارد");
+                        return View(model);
+                    }
+                }
+                #endregion
+                var limitsize = 0;
+                var destination = "Image\\Product\\" + model.Product.subCategory.Parent + "\\" + model.Product.subCategory.IdentifierName + "\\" + model.Product.Name;
+                var resault = await productLogic.UpdateProduct(model.Product);
+                if (resault)
+                {
+                    #region delete last files 
+                    foreach (var pp in productPhotoLogic.ProductPhotoList().Where(pp => pp.Product_Id == model.Product.Id).ToList())
+                    {
+                        //make complete path for delete file
+                        var path = destination + "\\" + pp.Name;
+                        //delete file from server
+                        #region delete last images from server
+                        var isSuccess = await fileManager.DeleteFile(path);
+                        if ((bool)isSuccess.First())
+                        {
+                            #region delete last images from db
+                            //delete photo from db
+                            var isDelete = await productPhotoLogic.DeleteProductPhoto(pp.Id);
+                            if (!isDelete)
+                            {
+                                ModelState.AddModelError("", "خطایی رخ داده است");
+                                return View(model);
+                            }
+                            #endregion
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", isSuccess.Last().ToString());
+                            return View(model);
+                        }
+                        #endregion
+                    }
+                    #endregion
+
+                    #region upload new image files
+                    foreach (var file in model.File)
+                    {
+                        //make uniqe name
+                        var ImageName = Guid.NewGuid().ToString();
+                        //upload image on server;
+                        var UploadFileResault = await fileManager.fileManager(ImageName, destination, limitsize, "", file);
+                        //if upload successfull 
+                        if ((bool)UploadFileResault.First() == true)
+                        {
+                            //convert bytes to kilobytes
+                            var ImageSize = file.Length / 1024;
+                            #region bind productphoto model
+                            ProductPhoto productPhoto = new ProductPhoto()
+                            {
+                                Name = ImageName + UploadFileResault.Last().ToString(),
+                                Size = (int)ImageSize,
+                                Product_Id = model.Product.Id,
+                                format = UploadFileResault.Last().ToString(),
+                            };
+                            #endregion
+                            //add photo to productphoto model
+                            await productPhotoLogic.AddProductPhoto(productPhoto);
+
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", UploadFileResault.Last().ToString());
+                            return View(model);
+                        }
+
+                    }
+                    #endregion
+                }
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("", "عملیات ویرایش با شکست مواجه شد");
+                return View(model);
+            }
+        }
+        [HttpGet("Product/DeleteProduct/Id")]
+        public async Task<IActionResult> DeleteProduct(string Id)
+        {
+            #region delete product
+            int PId = int.Parse(Id);
+            var product = productLogic.ProductDetail(PId);
+            var resualt = await productLogic.DeleteProduct(PId);
+            #endregion
+            return Json(new { name = product.Result.Name, Resualt = resualt });
         }
         public IActionResult AddOptions()
         {
