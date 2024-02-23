431479720c7362f71bdf43f1be0889348aac653f
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2024-02-23 19:13:42 +0330

    zoom box image view product

--- Electrical/wwwroot/js/function.js
+++ Electrical/wwwroot/js/function.js
@@ -28,3 +28,57 @@ function reverseContent(Identifier, NewContent) {
     var element = document.querySelector(Identifier);
     element.innerHTML = NewContent.toString();
 }
+function ZoomBoxImageView(Wrapper, prevBtn, nextBtn, slide) {
+    var wrapper = document.querySelector(Wrapper);
+    var wrapperchildrencount = wrapper.childElementCount;
+    var valueTranslateX = 0;
+    var direction = 0;
+    var counter = 1;
+    var zoom = 0.1;
+    $(wrapper).css("width", (wrapperchildrencount * 100) + "%")
+    $('.zoom-in').click(function () {
+        Zoom('.current', zoom);
+        zoom += 0.1;
+    })
+    $('.zoom-out').click(function () {
+        zoom -= 0.1;
+        Zoom('.current', zoom);
+    })
+    function Zoom(identifier, zoomValue) {
+        var image = document.querySelector(identifier);
+        $(image).css("transform", "scale(" + (1 + zoomValue) + ")");
+    }
+    function zoomReSet(identifier) {
+        var image = document.querySelector(identifier);
+        $(image).css("transform", "scale(1)");
+        console.log('reset');
+    }
+    function updateCurrentSlide() {
+        $(Wrapper + ' ' + slide).removeClass("current");
+        $(Wrapper).children().eq(counter - 1).addClass("current");
+    }
+    $(prevBtn).click(function () {
+        if (direction != -1) {
+            direction = -1;
+        }
+        if (counter > 1) {
+            zoomReSet('.current')
+            valueTranslateX -= 100;
+            $(wrapper).css("transform", "translateX(-" + valueTranslateX + "%)");
+            counter -= 1;
+            updateCurrentSlide();
+        }
+    })
+    $(nextBtn).click(function () {
+        if (direction != 1) {
+            direction = 1;
+        }
+        if (counter < wrapperchildrencount) {
+            zoomReSet('.current');
+            valueTranslateX += 100;
+            $(wrapper).css("transform", "translateX(-" + valueTranslateX + "%)");
+            counter += 1;
+            updateCurrentSlide();
+        }
+    })
+}
