fd43b75b6ea9891a21da2e5ec295a270ce63e9bf
MohammadmahdialmasiKH <133599241+MohammadmahdialmasiKH@users.noreply.github.com>
2024-03-21 07:39:48 +0430

    add persian calender extensions

---
+++ Utility/PersianDate/PersianDateExtension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Utility.PersianDate
+{
+    public static class PersianDateExtension
+    {
+        public static string ToPersianDate(this DateTime date)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            int year = pc.GetYear(date);
+            int month = pc.GetMonth(date);
+            int day = pc.GetDayOfMonth(date);
+
+            string persianDate = $"{day} {GetPersianMonthName(month)} {year}";
+            return persianDate;
+        }
+        private static string GetPersianMonthName(int month)
+        {
+            string[] monthName = new string[]
+            {
+                "فروردین",
+                "اردیبهشت",
+                "خرداد",
+                "تیر",
+                "مرداد",
+                "شهریور",
+                "مهر",
+                "آبان",
+                "آذر",
+                "دی",
+                "بهمن",
+                "اسفند",
+            };
+            return monthName[month - 1];
+        }
+    }
+}
